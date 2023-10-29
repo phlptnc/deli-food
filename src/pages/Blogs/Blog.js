8b40bc4b5cfc9d8260f1da4bf859f3545090133f
Philip Joseph <139831746+phlptnc@users.noreply.github.com>
2023-10-29 21:06:34 +0800

    Fixed blog title

--- src/pages/Blogs/Blog.js
+++ src/pages/Blogs/Blog.js
@@ -10,92 +10,94 @@ function Blog() {
     return (
         <>
             <section className="blog-section">
+                <h1>Blogs</h1>
                 <div className="body">
-                    <h1>Blogs</h1>
-                    <BlogBox />
-                </div>
-                <div className="side-bar">
-                    <div className="search-bar">
-                        <input type="text" placeholder="Search" />
-                        <div className="search-icon">
-                            <CgSearch className="magnifying-glass" />
-                        </div>
+                    <div className="wrapper">
+                        <BlogBox />
                     </div>
-                    <div className="popular-tags">
-                        <h2>Popular Tags</h2>
-                        <div className="tags">
-                            <p>#Burger</p>
-                            <p>#Cooking</p>
-                            <p>#Pizza</p>
-                            <p>#Drinks</p>
-                            <p>#Coffee</p>
-                            <p>#Pastry</p>
-                            <p>#Salad</p>
-                            <p>#Delivery</p>
-                            <p>#Favorites</p>
-                            <p>#Promo</p>
-                        </div>
-                    </div>
-                    <div className="categories">
-                        <h2>Categories</h2>
-                        <ul>
-                            <li>
-                                <p>
-                                    <FiChevronRight />
-                                    Burger
-                                </p>
-                                <span>(4)</span>
-                            </li>
-
-                            <li>
-                                <p>
-                                    <FiChevronRight />
-                                    Pasta
-                                </p>
-                                <span>(4)</span>
-                            </li>
-
-                            <li>
-                                <p>
-                                    <FiChevronRight />
-                                    Pastry
-                                </p>
-                                <span>(4)</span>
-                            </li>
-
-                            <li>
-                                <p>
-                                    <FiChevronRight />
-                                    Pizza
-                                </p>
-                                <span>(4)</span>
-                            </li>
-
-                            <li>
-                                <p>
-                                    <FiChevronRight />
-                                    Salad
-                                </p>
-                                <span>(4)</span>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="recent-posts">
-                        <h2>Recent Posts</h2>
-                        {blogData.map((post) => (
-                            <div className="post">
-                                <img
-                                    src={require("../../images/blogs/" +
-                                        post.id +
-                                        ".jpg")}
-                                    alt="post"
-                                />
-                                <div className="details">
-                                    <p>{post.date}</p>
-                                    <h3>{post.title}</h3>
-                                </div>
+                    <div className="side-bar">
+                        <div className="search-bar">
+                            <input type="text" placeholder="Search" />
+                            <div className="search-icon">
+                                <CgSearch className="magnifying-glass" />
                             </div>
-                        ))}
+                        </div>
+                        <div className="popular-tags">
+                            <h2>Popular Tags</h2>
+                            <div className="tags">
+                                <p>#Burger</p>
+                                <p>#Cooking</p>
+                                <p>#Pizza</p>
+                                <p>#Drinks</p>
+                                <p>#Coffee</p>
+                                <p>#Pastry</p>
+                                <p>#Salad</p>
+                                <p>#Delivery</p>
+                                <p>#Favorites</p>
+                                <p>#Promo</p>
+                            </div>
+                        </div>
+                        <div className="categories">
+                            <h2>Categories</h2>
+                            <ul>
+                                <li>
+                                    <p>
+                                        <FiChevronRight />
+                                        Burger
+                                    </p>
+                                    <span>(4)</span>
+                                </li>
+
+                                <li>
+                                    <p>
+                                        <FiChevronRight />
+                                        Pasta
+                                    </p>
+                                    <span>(4)</span>
+                                </li>
+
+                                <li>
+                                    <p>
+                                        <FiChevronRight />
+                                        Pastry
+                                    </p>
+                                    <span>(4)</span>
+                                </li>
+
+                                <li>
+                                    <p>
+                                        <FiChevronRight />
+                                        Pizza
+                                    </p>
+                                    <span>(4)</span>
+                                </li>
+
+                                <li>
+                                    <p>
+                                        <FiChevronRight />
+                                        Salad
+                                    </p>
+                                    <span>(4)</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="recent-posts">
+                            <h2>Recent Posts</h2>
+                            {blogData.map((post) => (
+                                <div className="post">
+                                    <img
+                                        src={require("../../images/blogs/" +
+                                            post.id +
+                                            ".jpg")}
+                                        alt="post"
+                                    />
+                                    <div className="details">
+                                        <p>{post.date}</p>
+                                        <h3>{post.title}</h3>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </section>
